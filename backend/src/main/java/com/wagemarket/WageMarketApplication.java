db748a2a37d689e41cd30be45b69e94d7649ec04
kaarelel <kaarelel@tlu.ee>
2025-04-24 11:54:36 +0300

    Initial commit for Vue and Spring Boot

---
+++ backend/src/main/java/com/wagemarket/WageMarketApplication.java
@@ -0,0 +1,13 @@
+package com.wagemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WageMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WageMarketApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wagemarket/WageMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WageMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
